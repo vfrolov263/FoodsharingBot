b942564646502e65ca0cde5de70a2111b622b66a
vfrolov <vladimirfrol@mail.ru>
2020-06-21 06:38:05 +0300

    Update bot.php

--- www/bot/bot.php
+++ www/bot/bot.php
@@ -5,10 +5,13 @@ function bot_sendMessage($user_id, $msg, $attachments) {
 }
 
 function bot_dialog($user_id, $user_msg) {
+	// Сообщения принимаются в формате: город, категория
 	if (strripos($user_msg, ", ") == false) return;
 	
+	// Выделяем слова
 	$mas = explode(", ", $user_msg);
 	
+	// Далее записываем город и категорию.
 	$city_file = fopen("city.txt", "w");
 	fwrite($city_file, $mas[0]);
 	fclose($city_file);
